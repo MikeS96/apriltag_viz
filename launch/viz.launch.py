d56a6710b08672e20048e71762ecc8ac6a4d2f01
MikeS96 <miguel.saaruiz@gmail.com>
2025-10-22 16:29:32 -0400

    patched problem with viz

--- launch/viz.launch.py
+++ launch/viz.launch.py
@@ -3,12 +3,12 @@ from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
-    composable_node = ComposableNode(node_name='viz', package='apriltag_viz', node_plugin='AprilVizNode')
+    composable_node = ComposableNode(name='apriltag_viz', package='apriltag_viz', plugin='AprilVizNode')
     container = ComposableNodeContainer(
-            node_name='viz_container',
-            node_namespace='apriltag',
+            name='viz_container',
+            namespace='apriltag',
             package='rclcpp_components',
-            node_executable='component_container',
+            executable='component_container',
             composable_node_descriptions=[composable_node],
             remappings=[("/apriltag/image", "/camera/image")],
             output='screen'
