7c46f404ae0e56b87e740a6d38becefec742851b
MikeS96 <miguel.saaruiz@gmail.com>
2025-10-22 18:07:06 -0400

    launch bug

--- launch/viz.launch.py
+++ launch/viz.launch.py
@@ -2,16 +2,19 @@ import launch
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
+
 def generate_launch_description():
-    composable_node = ComposableNode(name='apriltag_viz', package='apriltag_viz', plugin='AprilVizNode')
+    composable_node = ComposableNode(name="apriltag_viz", package="apriltag_viz", plugin="AprilVizNode")
     container = ComposableNodeContainer(
-            name='viz_container',
-            namespace='apriltag',
-            package='rclcpp_components',
-            executable='component_container',
-            composable_node_descriptions=[composable_node],
-            remappings=[("/apriltag/image", "/camera1/camera/color/image_raw")],
-            output='screen'
+        name="viz_container",
+        namespace="apriltag",
+        package="rclcpp_components",
+        executable="component_container",
+        composable_node_descriptions=[composable_node],
+        remappings=[
+            ("/image", "/camera1/camera/color/image_raw"),
+        ],
+        output="screen",
     )
 
     return launch.LaunchDescription([container])
